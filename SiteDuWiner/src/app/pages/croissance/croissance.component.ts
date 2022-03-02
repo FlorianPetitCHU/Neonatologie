65f8523188a938f85d5be15b2a1b5d2e4c02f747
HugoDasiilva <59052030+HugoDasiilva@users.noreply.github.com>
2022-03-02 13:54:30 +0100

    Début croissance

--- SiteDuWiner/src/app/pages/croissance/croissance.component.ts
+++ SiteDuWiner/src/app/pages/croissance/croissance.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./croissance.component.scss']
 })
 export class CroissanceComponent implements OnInit {
-
+  test = "";
   constructor() { }
 
   ngOnInit(): void {
