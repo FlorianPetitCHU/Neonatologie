af5b1edee091a58f4ccea0455bd673ba88d991b4
HugoDasiilva <Hugo.arbona@hotmail.fr>
2022-03-22 16:37:34 +0100

    gfdn

--- SiteDuWiner/src/app/pages/evol-med/evol-med.component.ts
+++ SiteDuWiner/src/app/pages/evol-med/evol-med.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {writeJsonFile} from 'write-json-file';
+
 
 
 @Component({
--- SiteDuWiner/src/app/pages/sortie/sortie.component.ts
+++ SiteDuWiner/src/app/pages/sortie/sortie.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sortie.component.scss']
 })
 export class SortieComponent implements OnInit {
-
+  secondSelectValue = null;
+  test = "";
+  Sortie = "";
+  val1 = "";
+  val2 = "";
+  val3 = "";
+  val4 = "";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+role : any;
+  role_med() {this.role = 1;
+  console.log(this.role);}
+  role_soignant() {this.role = 2;
+  console.log(this.role);}
 }
