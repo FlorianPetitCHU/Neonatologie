8e54e893256ba35470b343c315a2ae15ee220b9c
HugoDasiilva <Hugo.arbona@hotmail.fr>
2022-05-10 12:15:25 +0200

    Presentation soignant v2

--- SiteDuWiner/src/app/pages/soignant/grossesse-vue-soignant/grossesse-vue-soignant.component.ts
+++ SiteDuWiner/src/app/pages/soignant/grossesse-vue-soignant/grossesse-vue-soignant.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GrossesseVueSoignantComponent implements OnInit {
   secondSelectValue = null;
+  antibio = null;
+  thirdSelectValue = null;
   test = "";
   constructor() { }
   val1 = "";
